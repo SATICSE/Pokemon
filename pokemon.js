92cf69695e69e927b790c55ef40c2076b3452edc
CS-SATI <62654627+SATICSE@users.noreply.github.com>
2020-05-10 21:11:57 +0530

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,1 +1,7 @@
 document.body.style.border = "5px solid blue";
+    var img = new Image();
+    var div = document.getElementById('x');
+    img.onload = function() {
+      div.innerHTML += '<img src="'+img.src+'" />'; 
+    };
+    img.src = 'icons/pokemon.png';
